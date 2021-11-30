183707815e6e79a69d459f81ba2ee20086757dad
KimGN <rbska0314@gmail.com>
2021-11-30 17:55:08 +0900

    aaa

---
+++ src/main/java/com/breezelab/sample/model/UserProfile.java
@@ -0,0 +1,16 @@
+package com.breezelab.sample.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfile {
+    private String id;
+    private String name;
+    private String phone;
+    private String address;
+}
