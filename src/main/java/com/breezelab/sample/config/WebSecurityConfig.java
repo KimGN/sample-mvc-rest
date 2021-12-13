76511fa229acfa6c45e88f6f98453d8b674a8d52
KimGN <rbska0314@gmail.com>
2021-12-13 11:01:00 +0900

    security config

--- src/main/java/com/breezelab/sample/config/WebSecurityConfig.java
+++ src/main/java/com/breezelab/sample/config/WebSecurityConfig.java
@@ -11,21 +11,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                //user 경로는 로그인된 회원 누구나
+                .antMatchers("/user/**").authenticated()
+                //manamger 경로는 권한 가진 사람만 (manager or admin)
+                .antMatchers("/manamger/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                //admin 경로는 권한 가진 사람만 (admin)
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                //다른 경로는 접속 허용
+                .anyRequest().permitAll();
     }
 
     @Bean
